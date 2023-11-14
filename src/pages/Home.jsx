2ca9d7e514998dc8e2fca1d17bb85636a5f822f2
Regis <regis@MacBook-Air-de-Regis.local>
2023-11-14 22:21:46 +0100

    token

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,14 +22,14 @@ const Home = () => {
 
   return isLoading ? (
     <p>En cours de isLoading</p>
-  ) : (
+  ): (
     <body className="bodyHome">
       <div>
         {data.offers.map((offer) => {
-          const avatar = offer.owner.account.avatar?.secure_url;
+          const avatar = offer.owner.account.avatar?.secure_url})
 
           if (avatar === undefined) {
-            <>
+            <div>{
               return ( <p>{offer.owner.account.username} n'a pas de photo</p>
               <Link key={offer._id} to={`/offer/${offer._id}`}>
                 <p>{offer.owner.account.username}</p>
@@ -39,12 +39,12 @@ const Home = () => {
                 <p>{offer.product_details[1].TAILLE}</p>
                 <p>{offer.product_details[0].MARQUE}</p>
               </Link>
-              )
-            </>;
+          )}
+              </div>
           }
           if (avatar !== undefined) {
-            <>
-              return(
+            <div> 
+             {return(
               <p>{offer.owner.account.username} a pas de photo</p>
               <Link key={offer._id} to={`/offer/${offer._id}`}>
                 <p>{offer.owner.account.username}</p>
@@ -58,10 +58,12 @@ const Home = () => {
                 <p>{offer.product_details[1].TAILLE}</p>
                 <p>{offer.product_details[0].MARQUE}</p>
               </Link>
-              )
-            </>;
+              ) }</div>
+             
+              
           }
-        })}
+        
+        )}
       </div>
     </body>
   );
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,33 @@
-import { Form } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Signup = (manageToken) => {
+  const [email, setEmail] = (useState = "");
+  const [username, setUsername] = (useState = "");
+  const [password, setPassword] = (useState = "");
+  const [newsLetter, setNewsletter] = (useState = false);
+  const navigate = useNavigate();
+
+  const manageSubmiit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://lereacteur-vinted-api.herokuapp.com/user/signup",
+        {
+          email,
+          username,
+          password,
+          newsletter: newsLetter,
+        }
+      );
+      manageToken(response.data.token);
+      navigate("/");
+    } catch (error) {}
+  };
+
   return (
-    <form className="signupForm-container">
+    <form className="signupForm-container" onSubmit={manageSubmiit}>
       <h1>S'inscrire</h1>
       <input type="text" placeholder="email" />
       <input type="text" placeholder="username" />
